01ba3ff3b3ac480241424d6a727f7874d216c3e3
Sadab <sadabrahman275@gmail.com>
2022-10-25 01:25:52 +0600

    first commit

---
+++ src/Pages/Shared/SideNav/SideNav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SideNav = () => {
+    return (
+        <div>
+            <h2>Left Side Nav</h2>
+        </div>
+    );
+};
+
+export default SideNav;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBBIzYPzPwWW_Q6LVEKD-FOlxIZGNs0DK8",
+    authDomain: "programming-courses-1d3a3.firebaseapp.com",
+    projectId: "programming-courses-1d3a3",
+    storageBucket: "programming-courses-1d3a3.appspot.com",
+    messagingSenderId: "1045870170689",
+    appId: "1:1045870170689:web:78154bd8f1e4b268986cc0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
