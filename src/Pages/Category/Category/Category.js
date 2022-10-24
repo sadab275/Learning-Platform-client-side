cdd623714604f0e76b6021067edbc7ea058dff0b
Sadab <sadabrahman275@gmail.com>
2022-10-25 03:20:04 +0600

    courses sidenav added

--- src/Pages/Category/Category/Category.js
+++ src/Pages/Category/Category/Category.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import Courses from '../../Courses/Courses/Courses';
+import SideNav from '../../Shared/SideNav/SideNav';
 
 const Category = () => {
     return (
         <div>
             <h2>This is Category</h2>
+            <Container>
+
+                <Row>
+                    <Col lg="2">
+                        <SideNav></SideNav>
+                    </Col>
+                    <Col >
+                        <Courses></Courses>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 };
--- src/Pages/Shared/SideNav/SideNav.js
+++ src/Pages/Shared/SideNav/SideNav.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const SideNav = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/courses-categories')
+            .then(res => res.json())
+            .then(data => setCategories(data));
+    }, [])
+
     return (
         <div>
-            <h2>Left Side Nav</h2>
+            <h2>All Courses Category{categories.length}</h2>
+            <div>
+                {
+                    categories.map(category => <p key={category.id}>
+                        <Link to={`/category/${category.id}`}>{category.name}</Link>
+                    </p>)
+                }
+            </div>
         </div>
     );
 };
