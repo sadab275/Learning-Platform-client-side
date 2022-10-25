1973bab4afad9c6918cd4e8673599286fd19d2a1
Sadab <sadabrahman275@gmail.com>
2022-10-25 14:44:55 +0600

    navbar added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,10 +1,44 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import logo from '../../../assets/brands/logo.png'
 
 const Header = () => {
     return (
-        <div>
-            <h2>This is Header</h2>
-        </div>
+        <Navbar collapseOnSelect className='mb-4' expand="lg" bg="dark" variant="dark">
+            <Container>
+
+                <Navbar.Brand href="#home"><img alt="" src={logo} width="30" height="30" className="d-inline-block align-top"
+                />{' '} Programming Courses</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#features">Courses</Nav.Link>
+                        <Nav.Link href="#pricing">FAQ</Nav.Link>
+                        <Nav.Link href="#pricing">Blog</Nav.Link>
+                        {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">
+                                Another action
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">
+                                Separated link
+                            </NavDropdown.Item>
+                        </NavDropdown> */}
+                    </Nav>
+                    <Nav>
+                        <Nav.Link href="#deets">More deets</Nav.Link>
+                        <Nav.Link eventKey={2} href="#memes">
+                            Dank memes
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
 
