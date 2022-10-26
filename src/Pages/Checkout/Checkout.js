9278afbfe14aea51977272001e1912041dc84ed1
Sadab <sadabrahman275@gmail.com>
2022-10-26 22:55:41 +0600

    checkout page edited

--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 
 const Checkout = () => {
     const out = useLoaderData();
+    const { user } = useContext(AuthContext);
     console.log(out);
     return (
         <div className='container'>
             <h2>Welcome to the checkout page. </h2>
             <h1>You can now access to our course name : {out.name}</h1>
+            <p>Thanks for enrolling this course- <h3>{user?.displayName}</h3></p>
         </div>
     );
 };
