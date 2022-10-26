bf2cfcab9fd90ad6892dcddb4e52716909c25c0c
Sadab <sadabrahman275@gmail.com>
2022-10-26 13:39:27 +0600

    spinner added while refreshing the page

--- src/Routes/PrivateRoute/PrivateRoute.js
+++ src/Routes/PrivateRoute/PrivateRoute.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { useContext } from 'react';
+import { Spinner } from 'react-bootstrap';
 import { Navigate, useLocation } from 'react-router-dom';
 import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 
 const PrivateRoute = ({ children }) => {
 
-    const { user } = useContext(AuthContext);
+    const { user, loading } = useContext(AuthContext);
 
     const location = useLocation();
+
+    if (loading) {
+        return <Spinner className='mx-5' animation="grow" variant="info" />
+    }
 
     if (!user) {
         return <Navigate to='/login' state={{ from: location }} replace></Navigate>
