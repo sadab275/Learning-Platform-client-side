0e182b4cc152f1b1d32d178e25fcc298c6939288
Sadab <sadabrahman275@gmail.com>
2022-10-27 19:02:28 +0600

    pdf to scan

--- src/Pages/CourseDetails/CourseDetails.js
+++ src/Pages/CourseDetails/CourseDetails.js
@@ -3,14 +3,24 @@ import { Image } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { Link, useLoaderData } from 'react-router-dom';
+import { useReactToPrint } from 'react-to-print';
+import { useRef } from 'react';
 
 
 const CourseDetails = () => {
     const details = useLoaderData();
     console.log(details);
+    const componentRef = useRef();
+    const handlePrint = useReactToPrint({
+        content: () => componentRef.current,
+        documentTitle: 'emp-data',
+        onAfterPrint: () => alert('Downloaded')
+
+    })
     return (
-        <div>
-            <Card className="text-center mb-4">
+        <div >
+            <Button onClick={handlePrint}>Pdf Scan</Button>
+            <Card className="text-center mb-4" ref={componentRef} style={{ width: '100%', height: window.innerHeight }}>
                 <Card.Header>{details.heading}</Card.Header>
                 <Card.Body>
                     <Card.Title>{details.name}</Card.Title>
